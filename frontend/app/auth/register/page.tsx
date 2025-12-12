d495c8452fd28b6b3823214d1ad79aa114b0248d
Adikhairul123 <adikhairulanwar6@gmail.com>
2025-12-12 20:16:33 +0700

    adi: mengubah Post localhost:3000 menjadi localhost:3001 untuk mengalihkan ke backend

--- frontend/app/auth/register/page.tsx
+++ frontend/app/auth/register/page.tsx
@@ -15,10 +15,10 @@ export default function RegisterPage() {
     setLoading(true);
 
     try {
-      const res = await fetch("http://localhost:3000/api/auth/register", {
+      const res = await fetch("http://localhost:3001/api/auth/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ name, email, password }),
+        body: JSON.stringify({ email, password }),
       });
 
       const data = await res.json();
