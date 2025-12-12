adceb986e76e0eb7e320f68590b2d99598010f88
valbianalfikri0029 <valbianalfikri0029@gmail.com>
2025-12-12 14:07:03 +0700

    valbian: menambahkan Put dan Delete pada file route.ts

--- backend/app/api/user/route.ts
+++ backend/app/api/user/route.ts
@@ -82,3 +82,71 @@ export async function POST(request: NextRequest) {
         }, { status: 500 });
     }
 }
+
+// ========================================
+// PUT (Update user)
+// ========================================
+export async function PUT(request: NextRequest) {
+    try {
+        const user: UserRequest = await request.json();
+
+        if (!user.id) {
+            return NextResponse.json({
+                success: false,
+                message: "ID diperlukan"
+            }, { status: 400 });
+        }
+
+        const hash = bcrypt.hashSync(user.password, 10);
+
+        const update = await prisma.user.update({
+            where: { id: Number(user.id) },
+            data: {
+                email: user.email,
+                password: hash
+            }
+        });
+
+        return NextResponse.json({
+            meta_data: {
+                success: true,
+                message: "Data berhasil diperbarui",
+                status: 200
+            },
+            user: update
+        });
+
+    } catch (e) {
+        return NextResponse.json({
+            success: false,
+            message: "User tidak ditemukan"
+        }, { status: 404 });
+    }
+}
+
+// ========================================
+// DELETE (Hapus user)
+// ========================================
+export async function DELETE(request: NextRequest) {
+    try {
+        const { id }: { id: number } = await request.json();
+
+        await prisma.user.delete({
+            where: { id: Number(id) }
+        });
+
+        return NextResponse.json({
+            meta_data: {
+                success: true,
+                message: "Data berhasil dihapus",
+                status: 200
+            }
+        });
+
+    } catch (e) {
+        return NextResponse.json({
+            success: false,
+            message: "User tidak ditemukan"
+        }, { status: 404 });
+    }
+}
