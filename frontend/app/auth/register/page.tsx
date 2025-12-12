e0d9ea0dd45543a034a990e859bc06461c78864b
VGA2319 <gery_prananta@teknokrat.ac.id>
2025-12-12 20:07:35 +0700

    gery: hapus fungsi nama pada registrasi

--- frontend/app/auth/register/page.tsx
+++ frontend/app/auth/register/page.tsx
@@ -6,7 +6,6 @@ import { useRouter } from "next/navigation";
 
 export default function RegisterPage() {
   const router = useRouter();
-  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
