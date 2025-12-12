d980ba8402cbb29a898a90fd59f910fca0c062ed
valbianalfikri0029 <valbianalfikri0029@gmail.com>
2025-12-12 13:29:38 +0700

    valbian: migrasi table user

---
+++ backend/prisma/migrations/20251212062552_init/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "tb_user" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "tb_user_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "tb_user_email_key" ON "tb_user"("email");
