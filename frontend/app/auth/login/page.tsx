ed432a25bf009d5f3564b8c1ed65cb41cc71fd67
valbianalfikri0029 <valbianalfikri0029@gmail.com>
2025-12-14 14:27:24 +0700

    valbian: membuat file home.html untuk user yang sudah login

--- frontend/app/auth/login/page.tsx
+++ frontend/app/auth/login/page.tsx
@@ -14,7 +14,7 @@ export default function LoginPage() {
     setLoading(true);
 
     try {
-      const res = await fetch("http://localhost:3000/api/auth/login", {
+      const res = await fetch("http://localhost:3000/api/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
