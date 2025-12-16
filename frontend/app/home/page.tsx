5172191cebad31e6d926f6089e1102e7da453e65
VGA2319 <gery_prananta@teknokrat.ac.id>
2025-12-16 12:42:17 +0700

    gery: membuat kode digunakan untuk menuliskan CSS langsung di dalam file page.tsx

--- frontend/app/home/page.tsx
+++ frontend/app/home/page.tsx
@@ -3,6 +3,13 @@ import Link from "next/link";
 
 export default function HomePage() {
   return (
-    <></>
+    <>
+    {/* ================= INTERNAL CSS ================= */}
+      <style>{`
+        .about {
+          padding: 80px 20px;
+        }
+      `}</style>
+    </>
   )
 }
