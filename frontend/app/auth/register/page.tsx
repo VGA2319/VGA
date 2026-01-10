b30365cf88b4da0a805383d5dd11033285fe606f
valbianalfikri0029 <valbianalfikri0029@gmail.com>
2026-01-10 14:33:20 +0700

    valbian: mengedit ulang jalur regisrasi

--- frontend/app/auth/register/page.tsx
+++ frontend/app/auth/register/page.tsx
@@ -14,7 +14,7 @@ export default function RegisterPage() {
     setLoading(true);
 
     try {
-      const res = await fetch("/api/user", {
+      const res = await fetch("http://localhost:3000/api/user", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
