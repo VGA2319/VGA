48f86a3b03f5dd1ab7355e0b712c92f7b6716c67
valbianalfikri0029 <valbianalfikri0029@gmail.com>
2026-01-11 10:00:51 +0700

    Valbian: menambahkan endpoint GET booking dengan relasi user dan room

--- backend/app/api/bookingL/list/route.ts
+++ backend/app/api/bookingL/list/route.ts
@@ -1,2 +1,10 @@
 import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
+
+export async function GET() {
+  const bookings = await prisma.booking.findMany({
+    include: {
+      room: true,
+      user: true,
+    },
+  });
