90b360c685b14eb08da0b4514c50cb74fbd48e23
VGA2319 <gery_prananta@teknokrat.ac.id>
2026-01-13 10:32:18 +0700

    Gery: memperbaiki fungsi booking sebelum login tahap 2

--- frontend/app/booking/page.tsx
+++ frontend/app/booking/page.tsx
@@ -3,3 +3,14 @@ import Link from "next/link";
 
 export default function BookingListPage() {
   const rooms = [
+    {
+      id: "standard",
+      name: "Standard Room",
+      price: "500.000 / malam",
+      desc: "Kamar nyaman dengan fasilitas dasar.",
+    },
+  ];
+  return(
+
+  );
+}
