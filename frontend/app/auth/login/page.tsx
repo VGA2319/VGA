11fb5e86000187ef0908c9a3d12622107a5464e8
VGA2319 <gery_prananta@teknokrat.ac.id>
2026-01-13 10:40:34 +0700

    Gery: memperbaiki fungsi booking sebelum login tahap 13

--- frontend/app/auth/login/page.tsx
+++ frontend/app/auth/login/page.tsx
@@ -4,5 +4,8 @@ import { useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 
 export default function LoginPage() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
   
 }
